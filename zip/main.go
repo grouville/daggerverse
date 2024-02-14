cfde97bd4d5312974ea36bd0dbb4eabdf9f1243e
grouville <guillaume.derouville@gmail.com>
2024-02-13 17:57:15 -0800

    feat: improve compress + add usage
    Signed-off-by: grouville <guillaume.derouville@gmail.com>

--- zip/main.go
+++ zip/main.go
@@ -3,6 +3,7 @@ package main
 type Zip struct{}
 
 // zips the given directory and returns the path to the zip file
+// usage: dagger call compress --file <path> export --path <path.zip>
 func (m *Zip) Compress(file *File) *File {
 	filePath := "/tmp/file"
 
@@ -10,5 +11,6 @@ func (m *Zip) Compress(file *File) *File {
 		From("alpine:latest").
 		WithExec([]string{"apk", "add", "zip"}).
 		WithMountedFile(filePath, file).
-		WithExec([]string{"zip", "-r", filePath + ".zip", filePath}).File(filePath + ".zip")
+		WithExec([]string{"zip", "-r", filePath + ".zip", filePath}).
+		File(filePath + ".zip")
 }
